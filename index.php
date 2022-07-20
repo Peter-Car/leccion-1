f3e4a24e8a0efb2f8ab1469a5268d6c815ce5d73
Peter-Car <petercarpioa@gmail.com>
2022-07-20 15:34:00 -0500

    se sube el proyecto

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Calcular Salario</title>
+    </head>
+    
+    <body>
+       <style>
+            
+            header{
+                
+               background:#2ECC71;
+               
+   
+            }
+            
+            
+            #tabla{
+               
+                margin-left: 60px;
+                
+   
+            }
+            
+            .form-group{
+              
+                
+                width: 800px;
+                margin-right: 40px;
+                
+            }
+            
+            .container{
+                text-align: center;
+                margin-top: 30px;
+                margin-left: 40px;
+                display: flex;
+                font-size: 16px;
+                background: #2ECC71;
+            }
+            
+        </style>
+        <div class="container">
+            
+            <form>
+                
+                <div class="form-group">
+                    <p>
+                        <label>Empleado:</label> <br>
+                        <input type="text" id="nombre"  placeholder="Ingrese el empleado"> <br>
+                    </p>
+                     <p>
+                        <label>Cargo:</label> <br>
+                        <input type="text" id="cargo"  placeholder="Ingrese el cargo"> <br>
+                    </p>
+                     <p>
+                        <label>Valor/Hora:</label> <br>
+                        <input type="float" id="vhoras"  placeholder="Ingrese el valor por hora"> <br>
+                    </p>
+                     <p>
+                        <label>N°/Hora:</label> <br>
+                        <input type="float" id="nhoras"  placeholder="Ingrese el numero de horas"> <br>
+                    </p>
+                     <p>
+                        <label>Descripcion:</label> <br>
+                        <input type="text" id="nombre"  placeholder="Ingrese la descripcion"> <br>
+                    </p>
+                    
+                    <p>
+                        <button type="button" id="enviar" > Enviar Datos</button>
+                        
+                        
+                    </p>
+                    <input type="edidtext" id="enviar"  placeholder=""> <br>
+                    
+    </body>
+</html>
